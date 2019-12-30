76ba202b8a15aba52269d766b2ca4ce5720fe211
hehfey <31547044+hehfey@users.noreply.github.com>
2019-12-30 00:34:29 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const config = require("./config.json");
+const config = require("config.json");
 const axios = require("axios");
 
 client.on("ready", () => {
