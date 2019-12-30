6048396cb4089592c94be945f107a1accdcc7896
Jeff <jeffjohnstond@gmail.com>
2019-12-30 08:46:10 -0600

    Initial Commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json");
+const axios = require("axios");
+
+client.on("ready", () => {
+  console.log(
+    `Bot has started, with ${client.users.size} users, in ${
+      client.channels.size
+    } channels of ${client.guilds.size} guilds.`
+  );
+});
+
+client.on("message", async message => {
+  if (message.channel.id == "613136367219769391"){
+    if (message.author.bot) return;
+
+    if (message.content.indexOf(config.prefix) !== 0) return;
+
+    var args = message.content
+      .slice(config.prefix.length)
+      .trim()
+      .split(/ +/g);
+
+    const command = args.shift().toLowerCase();
+
+    if (command === "score") {
+      const type = args.join(" ").toString().replace(/[^\w\s!?]/g,'');
+      //message.reply(`Sure ${message.author}! Let me get you the score.`);
+      axios
+        .get("http://hehfey.com/webhooks/scrape.php?type=" + type)
+        .then(function(response) {})
+        .catch(function(error) {
+          console.log(error);
+        });
+    }
+
+    if (command === "purge") {
+      const deleteCount = parseInt(args[0], 10);
+      if (!deleteCount || deleteCount < 2 || deleteCount > 100)
+        return message.reply(
+          "Please provide a number between 2 and 100 for the number of messages to delete"
+        );
+
+      const fetched = await message.channel.fetchMessages({ limit: deleteCount });
+      message.channel
+        .bulkDelete(fetched)
+        .catch(error =>
+          message.reply(`Couldn't delete messages because of: ${error}`)
+        );
+    }
+  }
+});
+
+client.login(config.token);
